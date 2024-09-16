d685b4cccf11b5fb1ec4e82098af69d87b2ff14b
Asma S <saidiasma422@gmail.com>
2024-09-16 11:08:49 +0200

    add the construct function for the client class

--- client.php
+++ client.php
@@ -7,6 +7,14 @@ class Client {
     private $mail;
     private $tel;
     private $contrat;
+
+    public function __construct($nom,$prenom,$mail,$tel){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->mail = $mail;
+        $this->tel = $tel;
+
+    }
     
 
     // GET AND SET DE NOM
