ccb1b30ace134adac366136a8082574ed16bb4f1
Asma S <saidiasma422@gmail.com>
2024-09-16 10:55:11 +0200

    add get and set from the client class

--- client.php
+++ client.php
@@ -9,9 +9,64 @@ class Client {
     private $contrat;
     
 
+    // GET AND SET DE NOM
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    // GET AND SET DE PRENOM
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
 
 
+    // GET AND SET DE MAIL
+    public function getMail()
+    {
+        return $this->mail;
+    }
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
 
+        return $this;
+    }
 
+    // GET AND SET DE TEL
+    public function getTel()
+    {
+        return $this->tel;
+    }
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
 
+        return $this;
+    }
+
+    // GET AND SET DE CONTRAT
+    public function getContrat()
+    {
+        return $this->contrat;
+    }
+    public function setContrat($contrat)
+    {
+        $this->contrat = $contrat;
+
+        return $this;
+    }
 }
