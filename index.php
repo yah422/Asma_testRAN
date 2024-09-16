d10e661915f5ff755ba9111833935cdadac1282c
Asma S <saidiasma422@gmail.com>
2024-09-16 11:51:46 +0200

    add 3 differents class

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ require 'client.php';
 require 'voiture.php';
 require 'contrat.php';
 
-$nissan = new Voiture("nissan","qashkai","14FG32","disponible");
+// création de clients
+$client1 = new Client("Jean","Dujardin","jeandujardin12@gmail.com","0652102000");
 
-echo "Voiture : ". $nissan  ;
+
+// création de voitures
+$voiture1 = new Voiture("nissan","qashkai","14FG32","disponible");
+
+
+// création de contrats
+$contrat1 = new Contrat($client1,$voiture1,"16/10/25","25/10/25","300");
 
 
 
