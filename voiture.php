7d4865ab0987c85502320b13508ee271d1de0997
Asma S <saidiasma422@gmail.com>
2024-09-16 10:59:06 +0200

    add voiture class and the get and set

---
+++ voiture.php
@@ -0,0 +1,59 @@
+<?php
+
+class Voiture {
+
+    private $marque;
+    private $modele;
+    private $immatriculation;
+    private $disponibilite;
+
+    
+    // GET AND SET DE MARQUE
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+    public function setMarque($marque)
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    // GET AND SET DE MODELE
+    public function getModele()
+    {
+        return $this->modele;
+    }
+    public function setModele($modele)
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    // GET AND SET DE IMMATRICULATION
+    public function getImmatriculation()
+    {
+        return $this->immatriculation;
+    }
+    public function setImmatriculation($immatriculation)
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    // GET AND SET DE DISPONIBILITE
+    public function getDisponibilite()
+    {
+        return $this->disponibilite;
+    }
+    public function setDisponibilite($disponibilite)
+    {
+        $this->disponibilite = $disponibilite;
+
+        return $this;
+    }
+    
+}
